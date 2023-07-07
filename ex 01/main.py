929f5c837df57a080c9e8a6281db6d83ee5a656b
Luca <lucabattesini1@gmail.com>
2023-07-07 10:42:40 -0300

    ex01 My first exercise

--- ex 01/main.py
+++ ex 01/main.py
@@ -1,5 +1,7 @@
 import pandas as pd
 
+#My first Exercise
+
 notas = pd.Series([9, 2.3, 8, 9, 10])
 
 lst_matriculas = ['m1', 'm2', 'm3', 'm4', 'm5']
@@ -9,11 +11,3 @@ alunos = pd.Series(lst_nomes, index=lst_matriculas)
 print(notas)
 print('===================')
 print(alunos)
-
-#Data types:
-#values : vetor de dados;
-#index : vetor de rótulos;
-#name : nome do vetor de dados;
-#size : tamanho da Series (número de elementos);
-#index.name : nome do vetor de rótulos;
-#index.dtype : dtype do vetor de rótulos.
