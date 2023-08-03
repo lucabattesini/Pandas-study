a5a8d4f10db90a733be368d72055338c99ae8d02
Luca Battesini <93497971+lucabattesini@users.noreply.github.com>
2023-08-03 11:11:58 -0300

    ex14: exporting csv files

--- ex14/main.py
+++ ex14/main.py
@@ -1,5 +1,5 @@
 import pandas as pd
 
 #importar arquivos csv
-paises = pd.read_csv("C:\\Users\\Win\Documents\\Estudos-Progrmção\\Estudos\\Pandas\\exemplos\\Pandas-study-main\\ex14\\bfotool-download.csv", index_col="sigla")
+paises = pd.read_csv("C:\\Users\\Win\Documents\\Estudos-Progrmção\\Estudos\\Pandas\\exemplos\\Pandas-study-main\\ex14\\bfotool-download.csv", index_col="particles")
 print(paises)
