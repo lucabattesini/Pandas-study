cc969531249a443ca58f687e693f9b39cfa6d782
Luca Battesini <93497971+lucabattesini@users.noreply.github.com>
2023-08-04 11:22:03 -0300

    ex15: Importing csv and separating with ','

---
+++ ex15/main.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+#Importando csv sem cabeçalho separado por virgulas
+notas = pd.read_csv("C:\\Users\\Win\Documents\\Estudos-Progrmção\\Estudos\\Pandas\\exemplos\\Pandas-study-main\\ex15\\bfotool-download.csv", sep=";",
+                    names=['matricula', 'nota', 'nota1'])
+print(notas)
