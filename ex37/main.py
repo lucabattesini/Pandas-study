c7550e41dfe476f90959afec2f7d02d5c6bed2b8
Luca Battesini <93497971+lucabattesini@users.noreply.github.com>
2023-09-05 16:29:42 -0300

    ex37: Inter junction

--- ex37/main.py
+++ ex37/main.py
@@ -1,2 +1,28 @@
 import pandas as pd
 
+#Fazendo junção interna
+#Data Frame
+dic_depto = {"id":["D1","D2","D3","D4"], 
+ "nomDepto": ["Compras","RH","TI","Vendas"],
+ "local":["SP","RJ","RJ","SP"]
+ }
+dic_emp = {"num":[3199,3269,3555,3788,3844],
+ "nome": ["Ana","David","José","Marina","Luís"],
+ "salario":[1600,2975,1500,5000,3000],
+ "idDepto": ["D2","D3",None,"D2","D4"]
+ }
+depto = pd.DataFrame(dic_depto)
+emp = pd.DataFrame(dic_emp)
+
+#Operação de junção
+juncao_interna = pd.merge(emp, depto, left_on="idDepto",
+right_on="id")
+print('------------------------------')
+print("depto:")
+print(depto)
+print('------------------------------')
+print("emp:")
+print(emp)
+print('------------------------------')
+print("junção interna:")
+print(juncao_interna)
